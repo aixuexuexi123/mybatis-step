225e27f0667c64d0e134efe29687036a9d63a1f7
m0_75137040 <2190487384@qq.com>
2024-04-11 12:10:11 +0800

    Initial commit

---
+++ src/main/java/mybatisStep/MybatisStepApplication.java
@@ -0,0 +1,13 @@
+package mybatisStep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisStepApplication.class, args);
+    }
+
+}
